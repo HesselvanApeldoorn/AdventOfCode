122bb7025136230a99fe6dcc67831b243c34c55a
Hessel van Apeldoorn <hesselvanapeldoorn@gmail.com>
2017-12-09 14:20:58 +0100

    added first part day 3

---
+++ day3/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+func main() {
+	fmt.Printf("hello, world\n")
+	fmt.Print("gib input: ")
+	var input int
+	fmt.Scan(&input)
+	i := 1
+	for i * i < input {
+		i = i + 2
+	}
+	ringIndex := i / 2
+	s := fmt.Sprintf("Aantal stappen naar binnen: %d. ", ringIndex)
+	fmt.Println(s)
+	zuid := i*i-ringIndex
+	west := i*i-3*ringIndex
+	noord := i*i-5*ringIndex
+	oost := i*i-7*ringIndex
+	s2 := fmt.Sprintf("Dichtsbijzijnde in zelfde ring: %d, %d, %d, %d", zuid, west, noord, oost)
+	fmt.Println(s2)
+	dichtsbijzijnde := math.MaxUint32
+	for j:=0; j<4; j++ {
+		if math.Abs(float64(input - (i*i-j*2*ringIndex-ringIndex))) < float64(dichtsbijzijnde) {
+			dichtsbijzijnde =  int(math.Abs(float64(input - (i*i-j*2*ringIndex-ringIndex))))
+		}
+	}
+	s3 := fmt.Sprintf("Dichtsbijzijnde: %d", dichtsbijzijnde)
+	fmt.Println(s3)
+	aantalStappen := ringIndex + dichtsbijzijnde
+	s4 := fmt.Sprintf("Aantal stappen: %d", aantalStappen)
+	fmt.Println(s4)
+}
