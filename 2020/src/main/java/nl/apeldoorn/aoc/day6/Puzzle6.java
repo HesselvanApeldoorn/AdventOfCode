d8b5995605d4507e8ad454e51060111bbce07fc2
Hessel van Apeldoorn <hesselvanapeldoorn@gmail.com>
2020-12-06 11:06:04 +0100

    added day6

---
+++ 2020/src/main/java/nl/apeldoorn/aoc/day6/Puzzle6.java
@@ -0,0 +1,54 @@
+package nl.apeldoorn.aoc.day6;
+
+import lombok.NoArgsConstructor;
+import nl.apeldoorn.aoc.AbstractPuzzle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+public class Puzzle6 extends AbstractPuzzle {
+
+	@Override
+	public long solveFirst() {
+		ArrayList<Set<Character>> cleanedLines = new ArrayList<>();
+		Set<Character> tempLine = new HashSet<>();
+		for(int i=0; i<inputLines.length; i++) {
+			tempLine.addAll(inputLines[i].chars().mapToObj(c -> (char) c).collect(Collectors.toList()));
+			if (inputLines[i].isEmpty()) {
+				cleanedLines.add(tempLine);
+				tempLine = new HashSet<>();
+			}
+		}
+		return cleanedLines.stream().flatMap(Collection::stream).count();
+	}
+
+	@Override
+	protected long solveSecond() {
+		ArrayList<Set<Character>> cleanedLines = new ArrayList<>();
+		Set<Character> tempLine = new HashSet<>();
+		boolean newEntry = true;
+		for(int i=0; i<inputLines.length; i++) {
+			if (inputLines[i].isEmpty()) {
+				cleanedLines.add(tempLine);
+				tempLine = new HashSet<>();
+				newEntry = true;
+			} else {
+				if (newEntry) {
+					newEntry = false;
+					tempLine.addAll(inputLines[i].chars().mapToObj(c -> (char) c).collect(Collectors.toList()));
+				} else {
+					tempLine.retainAll(inputLines[i].chars().mapToObj(c -> (char) c).collect(Collectors.toList()));
+				}
+			}
+		}
+		return cleanedLines.stream().flatMap(Collection::stream).count();
+	}
+
+	public static void main(String args[]) {
+		new Puzzle6().solve();
+	}
+}
